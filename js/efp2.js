6f3caec7eb7e067f8a5ee60611e87f3fb39169a4
warrenson <polarisinc@gmail.com>
2016-02-05 23:46:09 +1300

    add: inital web interface files

---
+++ js/efp2.js
@@ -0,0 +1,17 @@
+// efp2.js
+// eFP2 View Interface Script
+//
+// calls perl CGIs in ./perl/:
+//      autocomplete.pl
+//      init_experiment.pl
+//      process_componenet.pl
+
+
+// On douement load, initialise the default Experiment
+$(document).ready(function(){
+        $("#test").click(function(){
+                    $(this).css('background-color', 'red');
+                        });
+});
+
+
